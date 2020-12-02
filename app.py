cf2ae56a151b2464cc3d141e11462a751c55fa5a
Eric S Penrose <ericspenrose@gmail.com>
2020-12-01 19:44:40 -0600

    Troubleshoot

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 #%%
 from flask import Flask, render_template, redirect
 import pymongo
-from scrape_mars import scrape, def_db
 import os
 #%%
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
 db = client.MuskDB
 app = Flask(__name__)
-def_db()
 
 #%%
 @app.route("/")
 def musk():
-    return render_template("templates/index.html")
+    return render_template("static/templates/index.html")
     
 @app.route("/scrape")
 def happy():
+    pass
 
 
-# %%
+if __name__ == "__main__":
+    app.run(debug=True)
