7cc73ec9c243e2ce9c627486796b6624bdab82e2
Eric S Penrose <ericspenrose@gmail.com>
2020-12-03 19:54:06 -0600

    Update logic.js

--- static/JS/logic.js
+++ static/JS/logic.js
@@ -1,1 +1,7 @@
+//var shapes for drawing country outlines
+//var elon for elon data
+//var years for years
+//var countries for country list
+//var happy for default response variable
+// vars freedom and GDP for alternate response variables
 console.log(shapes)
