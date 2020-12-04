88b94ccd5b8c14c384ee4e88d2f429d94cae8068
Eric S Penrose <ericspenrose@gmail.com>
2020-12-03 21:05:46 -0600

    last OCD

--- static/JS/logic.js
+++ static/JS/logic.js
@@ -3,5 +3,7 @@
 //var years for years
 //var countries for country list
 //var happy for default response variable
-// vars freedom and GDP for alternate response variables
+//vars freedom and GDP for alternate response variables
 console.log(shapes)
+console.log(freedom)
+console.log(elon)
